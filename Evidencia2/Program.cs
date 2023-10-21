6aa9bcc3ea8fa81a2e6f224bae743c1b3f4c461c
Rick <rick1304.25@gmail.com>
2023-10-21 16:14:11 -0600

    Implementación de documentación.

--- Evidencia2/Program.cs
+++ Evidencia2/Program.cs
@@ -1,23 +1,31 @@
-﻿using Microsoft.VisualBasic.CompilerServices;
-using Utils = Evidencia2.Utils;
+﻿using Utils = Evidencia2.Utils;
 
+// Se inicializan los datos de Anibal y María
 var data = new[]
 {
     new[]
     {
-        new double[]{
+        // Valores de entrada de Anibal
+        new double[]
+        {
             1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
         },
-        new double[]{
+        // Valores de salida de Anibal
+        new double[]
+        {
             50, 55, 60, 61, 65, 67, 69, 70, 72, 73, 74, 76
         }
     },
     new[]
     {
-        new double[]{
+        // Valores de entrada de María
+        new double[]
+        {
             1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
         },
-        new double[]{
+        // Valores de salida de Anibal
+        new double[]
+        {
             49, 57, 59, 61, 63, 65, 67, 69, 70, 71, 72, 74
         }
     }
@@ -25,11 +33,15 @@ var data = new[]
 
 var anibal = new[]
 {
-    new Func<double, double>[]{
+    // Funciones para los datos de Anibal
+    new Func<double, double>[]
+    {
         Math.Log,
         _ => 1
     },
-    new Func<double, double>[]{
+    // Derivadas de las funciones
+    new Func<double, double>[]
+    {
         t => 1 / t,
         _ => 0
     }
@@ -37,12 +49,17 @@ var anibal = new[]
 
 var maria = new[]
 {
-    new Func<double, double>[]{
+    // Funciones para los datos de María
+    new Func<double, double>[]
+    {
         t => Math.Cos(t / 8),
         t => Math.Exp(t / 10)
     },
-    new Func<double, double>[]{
+    // Derivadas de las funciones
+    new Func<double, double>[]
+    {
         t => -Math.Sin(t / 8) / 8,
         t => Math.Exp(t / 10) / 10
     }
 };
+
--- Evidencia2/Utils.cs
+++ Evidencia2/Utils.cs
@@ -2,13 +2,28 @@
 
 namespace Evidencia2;
 
+/// <summary>
+/// Funciones útiles.
+/// <remarks>
+/// La clase incluye funciones para matrices, como impresión y producto. También incluye función para ajuste de curva.
+/// </remarks>
+/// </summary>
 public static class Utils
 {
+    /// <summary>
+    /// Regresa los coeficientes de las funciones para que se ajusten a los datos proporcionados.
+    /// </summary>
+    /// <param name="input">Datos de entrada.</param>
+    /// <param name="output">Datos de salida esperados.</param>
+    /// <param name="funcs">Funciones de la ecuación.</param>
+    /// <returns>Coeficientes de las funcioens.</returns>
     public static double[] LinearFit(double[] input, double[] output, Func<double, double>[] funcs)
     {
         var jacobian = new double[input.Length, funcs.Length];
         var size = new int[] { jacobian.GetLength(0), jacobian.GetLength(1) };
 
+        // Se llena la matriz jacobiana
+        // Las filas indican el valor de entrada que se utiliza, mientras la columna indica la función que se utiliza.
         for (var i = 0; i < size[0]; i++)
         {
             for (var j = 0; j < size[1]; j++)
@@ -19,9 +34,16 @@ public static class Utils
 
         var tJacobian = Transpose(jacobian);
         var augMatrix = new double[size[1], size[1] + 1];
-        augMatrix = copyTo(DotProduct(tJacobian, jacobian), augMatrix);
-        augMatrix = copyTo(DotProduct(tJacobian, Transpose(ToMatrix(output))), augMatrix, startColumn: size[1]);
+        
+        // Se copia los valores del producto punto de la jacobiana por su transpuesta en la matriz aumentada.
+        augMatrix = CopyTo(DotProduct(tJacobian, jacobian), augMatrix);
+        
+        // Se copian los valores del producto punto de la matriz jacobiana transpuesta con la matriz transpuesta de 
+        // los valores de salida a la matriz aumentada, colocándolos en la última columna de la matriz
+        augMatrix = CopyTo(DotProduct(tJacobian, Transpose(ToMatrix(output))), augMatrix, startColumn: size[1]);
+        
         augMatrix = GaussianElimination(augMatrix);
+        // La cantidad de coeficientes es igual a la cantidad de funciones
         var coefficients = new double[size[1]];
         for (int i = 0; i < size[1]; i++)
         {
@@ -30,9 +52,16 @@ public static class Utils
 
         return coefficients;
     }
-
+    
+    /// <summary>
+    /// Realiza el producto punto de dos matrices.
+    /// </summary>
+    /// <param name="matrixOne">Matriz uno.</param>
+    /// <param name="matrixTwo">Matriz dos.</param>
+    /// <returns>Resultado del producto punto.</returns>
     public static double[,] DotProduct(double[,] matrixOne, double[,] matrixTwo)
     {
+        // Se guardan las dimensiones
         var measures = new[]
         {
             matrixOne.GetLength(0),
@@ -45,6 +74,8 @@ public static class Utils
         {
             for (var j = 0; j < measures[1]; j++)
             {
+                // Cada posición es igual a la suma del producto de la fila de la primera matriz, por la columna de la
+                // segunda matriz (producto elemento por elemento)
                 for (var k = 0; k < measures[2]; k++)
                 {
                     result[i, j] += matrixOne[i, k] * matrixTwo[k, j];
@@ -54,7 +85,12 @@ public static class Utils
 
         return result;
     }
-
+    
+    /// <summary>
+    /// Transpone una matriz.
+    /// </summary>
+    /// <param name="matrix">Matriz.</param>
+    /// <returns>Matriz transpuesta.</returns>
     public static double[,] Transpose(double[,] matrix)
     {
         var size = new[] { matrix.GetLength(0), matrix.GetLength(1) };
@@ -64,33 +100,52 @@ public static class Utils
         {
             for (var j = 0; j < size[0]; j++)
             {
+                // Se invierten las coordenadas
                 result[i, j] = matrix[j, i];
             }
         }
 
         return result;
     }
-
-    public static double[,] copyTo(double[,] src, double[,] destination, int startRow = 0, int startColumn = 0)
+    
+    /// <summary>
+    /// Copia los elementos de una matriz en otra.
+    /// </summary>
+    /// <param name="src">Matriz fuente.</param>
+    /// <param name="destination">Matriz destino.</param>
+    /// <param name="startRow">Fila donde inicia el pegado.</param>
+    /// <param name="startColumn">Columna donde inicia el pegado.</param>
+    /// <returns>Matriz con los valores pegados.</returns>
+    public static double[,] CopyTo(double[,] src, double[,] destination, int startRow = 0, int startColumn = 0)
     {
         var size = new[] { src.GetLength(0), src.GetLength(1) };
-        
+
         for (var i = 0; i < size[0]; i++)
         {
             for (var j = 0; j < size[1]; j++)
             {
+                // Copia los elementos de la matriz fuente a la matriz resultante, iniciando el pegado
+                // desde la fila y columna iniciales.
                 destination[i + startRow, j + startColumn] = src[i, j];
             }
         }
 
         return destination;
     }
-
+    
+    /// <summary>
+    /// Convierte un arreglo en una matriz.
+    /// </summary>
+    /// <remarks>
+    /// La cantidad de filas será 1, y la cantidad de columnas será el tamaño del arreglo.
+    /// </remarks>
+    /// <param name="array">Arreglo.</param>
+    /// <returns>Matriz del arreglo.</returns>
     public static double[,] ToMatrix(double[] array)
     {
         int size = array.Length;
         double[,] result = new double[1, size];
-        
+
         for (int i = 0; i < size; i++)
         {
             result[0, i] = array[i];
@@ -98,7 +153,7 @@ public static class Utils
 
         return result;
     }
-    
+
     /// <summary>
     /// Realiza una eliminación gaussiana a la matriz introducida.
     /// </summary>
@@ -138,19 +193,29 @@ public static class Utils
                 }
             }
         }
+
         // Retorno de la matriz extendida resultante
         return augmentedMatrix;
     }
 
+    /// <summary>
+    /// Regresa la representación en texto de un arreglo.
+    /// </summary>
+    /// <param name="array">Arreglo.</param>
+    /// <returns>Representación en texto del arreglo.</returns>
     public static string ToString(double[] array)
     {
+        // StringBuilder para formar el texto
         StringBuilder text = new StringBuilder();
+        // Se guarda el tamaño del arreglo
         var size = array.GetLength(0);
+        // Se realiza un for para ir guardando los valores
         for (var i = 0; i < size; i++)
         {
             text.Append(array[i] + (i == size - 1 ? "" : ", "));
         }
 
+        // Se regresa el texto
         return text.ToString();
     }
 }
